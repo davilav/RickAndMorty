cf93101194490fcb1095d1583be9da7e37f2d343
David Felipe Avila <davilav@unal.edu.co>
2023-09-29 16:36:00 -0500

    Room Database has been implemented

---
+++ app/src/main/java/com/dfavilav/zararickmorty/di/DatabaseModule.kt
@@ -0,0 +1,28 @@
+package com.dfavilav.zararickmorty.di
+
+import android.content.Context
+import androidx.room.Room
+import com.dfavilav.zararickmorty.data.local.RickAndMortyDatabase
+import com.dfavilav.zararickmorty.util.Constants.RAM_DATABASE
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): RickAndMortyDatabase {
+        return Room.databaseBuilder(
+            context,
+            RickAndMortyDatabase::class.java,
+            RAM_DATABASE
+        ).build()
+    }
+}
--- app/src/main/java/com/dfavilav/zararickmorty/util/Constants.kt
+++ app/src/main/java/com/dfavilav/zararickmorty/util/Constants.kt
@@ -2,4 +2,5 @@ package com.dfavilav.zararickmorty.util
 
 object Constants {
     const val BASE_URL = "https://rickandmortyapi.com/"
+    const val RAM_DATABASE = "ram_database"
 }
