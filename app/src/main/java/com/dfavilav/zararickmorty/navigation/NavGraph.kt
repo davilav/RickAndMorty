21322aac0c97152c108f7cb70827a5febd72e3a6
David Felipe Avila <davilav@unal.edu.co>
2023-09-30 12:28:32 -0500

    Splash Screen created with Compose and Navigation

---
+++ app/src/main/java/com/dfavilav/zararickmorty/navigation/NavGraph.kt
@@ -0,0 +1,25 @@
+package com.dfavilav.zararickmorty.navigation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import coil.annotation.ExperimentalCoilApi
+import com.dfavilav.zararickmorty.presentation.screens.SplashScreen
+
+@ExperimentalMaterialApi
+@ExperimentalCoilApi
+@ExperimentalAnimationApi
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Splash.route
+    ) {
+        composable(route = Screen.Splash.route) {
+            SplashScreen(navController = navController)
+        }
+    }
+}
