818f3eeb804287e77aef3aed16b20b8f7c38b5d7
David Felipe Avila <davilav@unal.edu.co>
2023-09-30 11:05:27 -0500

    Firebase has been implemented

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,7 @@ plugins {
     alias(libs.plugins.com.google.devtools.ksp) apply false
     alias(libs.plugins.org.jetbrains.kotlin.plugin.serialization) apply false
     alias(libs.plugins.com.google.dagger.hilt.android) apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.google.firebase.crashlytics") version "2.9.1" apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
